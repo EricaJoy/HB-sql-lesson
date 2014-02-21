5be6b3cd591b2f9dc3c15384f10c7c449ead415b
Hackbright Student <info@hackbrightacademy.com>
2014-02-21 11:32:47 -0800

    Basic functions here, time to add more.

--- hackbright_app.py
+++ hackbright_app.py
@@ -3,6 +3,8 @@ import sqlite3
 DB = None
 CONN = None
 
+### DB Query Functions ###
+
 def get_student_by_github(github):
     query = """SELECT first_name, last_name, github FROM Students WHERE github = ?"""
     DB.execute(query, (github,))
@@ -11,10 +13,21 @@ def get_student_by_github(github):
 Student: %s %s
 Github account: %s"""%(row[0], row[1], row[2])
 
+def make_new_student(first_name, last_name, github):
+    query = """INSERT INTO Students VALUES (?, ?, ?)"""
+    DB.execute(query, (first_name, last_name, github)) # Is there any situation where DB.execute doesn't take a tuple?
+    CONN.commit()
+    print "Successfully added student: %s %s"%(first_name, last_name)
+
+
+
+### Connect to the database ###
 def connect_to_db():
     global DB, CONN
     CONN = sqlite3.connect("hackbright.db")
     DB = CONN.cursor()
+
+### End connect to the database ###
 
 def main():
     connect_to_db()
@@ -24,6 +37,9 @@ def main():
         tokens = input_string.split()
         command = tokens[0]
         args = tokens[1:]
+
+        # TODO around here somewhere: Echo user input back, ask if it looks good
+        # then commit if they say yes. Then balete lines 17-18.
 
         if command == "student":
             get_student_by_github(*args) 
