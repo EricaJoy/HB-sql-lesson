98414e70d2e5115f89ae66a138d5f9d63e9e5695
Hackbright Student <info@hackbrightacademy.com>
2014-02-21 12:02:21 -0800

    (Untested) db write functions added. db reads next.

--- hackbright_app.py
+++ hackbright_app.py
@@ -3,7 +3,48 @@ import sqlite3
 DB = None
 CONN = None
 
+### schema ###
+
+# This is just here for reference while writing queries
+
+# CREATE TABLE Grades (student_github varchar(30), project_title varchar(30), grade INT);
+# CREATE TABLE Projects (id INTEGER PRIMARY KEY AUTOINCREMENT, title varchar(30), description TEXT, max_grade INT);
+# CREATE TABLE Students (first_name varchar(30), last_name varchar(30), github varchar(30));
+### end schema ###
+
+
+
 ### DB QUERY FUNCTIONS ###
+
+### ** DB WRITES ** ### 
+
+# Add a student
+def make_new_student(first_name, last_name, github):
+    query = """INSERT INTO Students VALUES (?, ?, ?)"""
+    DB.execute(query, (first_name, last_name, github)) # Is there any situation where DB.execute doesn't take a tuple?
+    CONN.commit()
+    print "Successfully added student: %s %s"%(first_name, last_name)
+
+# Add a project
+def add_a_project(title, description, max_grade):
+    query = """INSERT INTO Projects VALUES (?, ?, ?)"""
+    DB.execute(query, (title, description, max_grade))
+    CONN.commit()
+    print "Successfully added project: %s: %s with a maximum grade of %d"%(title, description, max_grade)
+
+
+# Give a grade to a student
+def give_student_grade(github, project, grade):
+    query = """INSERT INTO Grades VALUES (?, ?, ?)"""
+    DB.execute(query, (github, project, grade))
+    CONN.commit()
+    print "Successfully added grade %d for %s to %s."%(grade, project, github)
+
+### ** END DB WRITES ** ### 
+
+
+
+### ** DB READS ** ### 
 
 def get_student_by_github(github):
     query = """SELECT first_name, last_name, github FROM Students WHERE github = ?"""
@@ -13,33 +54,27 @@ def get_student_by_github(github):
 Student: %s %s
 Github account: %s"""%(row[0], row[1], row[2])
 
-# Add a student
-def make_new_student(first_name, last_name, github):
-    query = """INSERT INTO Students VALUES (?, ?, ?)"""
-    DB.execute(query, (first_name, last_name, github)) # Is there any situation where DB.execute doesn't take a tuple?
-    CONN.commit()
-    print "Successfully added student: %s %s"%(first_name, last_name)
-
 # Query for projects by title
 def project_by_title():
-    pass
-
-# Add a project
-def add_a_project():
-    pass
-
+    query = """ """
+    DB.execute(query, (args))
+    print """\
+    """
 # Query for a student's grade given a project
 def student_grade_by_project():
-    pass
-
-# Give a grade to a student
-def give_student_grade():
-    pass
+    query = """ """
+    DB.execute(query, (args))
+    print """\
+    """
 
 # Show all the grades for a student
 def show_all_student_grades():
-    pass
+    query = """ """
+    DB.execute(query, (args))
+    print """\
+    """
 
+### ** END DB READS ** ### 
 
 ### END DB QUERY FUNCTIONS ###
 
@@ -60,8 +95,9 @@ def main():
         command = tokens[0]
         args = tokens[1:]
 
-        # TODO around here somewhere: Echo user input back, ask if it looks good
-        # then commit if they say yes. Then balete lines 17-18.
+        # TODO around here somewhere: If db read, echo user input back,
+        # ask if it looks good then commit if they say yes. Move commits out of
+        # functions.
 
         if command == "student":
             get_student_by_github(*args) 
